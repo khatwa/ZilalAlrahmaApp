880ea959b40f9654f92a60feca473c1b70493546
Simbaadeeb <38065309+Simbaadeeb@users.noreply.github.com>
2019-08-30 15:13:21 +0200

    Update SIMManager.kt
    Deleted unnecessary comments

--- app/src/main/java/com/khatwa/zilalalrahmaapp/SIMManager.kt
+++ app/src/main/java/com/khatwa/zilalalrahmaapp/SIMManager.kt
@@ -15,39 +15,12 @@ import java.lang.Exception
 
 class SIMManager(context: Context) {
 
-//    lateinit var imeiSIM1:String
-//    lateinit var imeiSIM2:String
     var SIM1OperatorName:String? = null
     var SIM2OperatorName:String? = null
     var isSIM1Ready = false
     var isSIM2Ready = false
     var isDualSIM = false
 
-//    fun getImeiSIM1():String {
-//        return imeiSIM1
-//    }
-//
-//    fun getImeiSIM2():String {
-//        return imeiSIM2
-//    }
-//
-//    public String getSIM1OperatorName() {
-//        return SIM1OperatorName;
-//    }
-//
-//    public String getSIM2OperatorName() {
-//        return SIM2OperatorName;
-//    }
-//
-//    public boolean isSIM1Ready() {
-//        return isSIM1Ready;
-//    }
-//
-//    public boolean isSIM2Ready() { return isSIM2Ready; }
-//
-//    public boolean isDualSIM() {
-//        return isDualSIM;
-//    }
 
     init {
 
@@ -177,147 +150,9 @@ class SIMManager(context: Context) {
             }
 
         }
-
-//        Toast.makeText(context, "SIM1: $SIM1OperatorName, SIM2: $SIM2OperatorName", Toast.LENGTH_LONG).show()
     }
 
-
-//    public static SIMManager getInstance(Context context) {
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-//            if(simManager == null) {
-//                simManager = new SIMManager();
-//                SubscriptionManager subscriptionManager = (SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
-//
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                    if (context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-//                        List activeSubscriptionInfoList = subscriptionManager.getActiveSubscriptionInfoList();
-//                        if (activeSubscriptionInfoList.size() >= 2) {
-//                            simManager.isDualSIM = true;
-//
-//                            try {
-//                                SubscriptionInfo sim1 = (SubscriptionInfo) activeSubscriptionInfoList.get(0);
-//                                sim1.getSimSlotIndex();
-//                                simManager.isSIM1Ready = true;
-//                                simManager.SIM1OperatorName = (String) sim1.getDisplayName();
-//                            } catch (Exception e) {
-//                                e.printStackTrace();
-//                            }
-//
-//                            try {
-//                                SubscriptionInfo sim2 = (SubscriptionInfo) activeSubscriptionInfoList.get(1);
-//                                sim2.getSimSlotIndex();
-//                                simManager.isSIM1Ready = true;
-//                                simManager.SIM2OperatorName = (String) sim2.getDisplayName();
-//                            } catch (Exception e) {
-//                                e.printStackTrace();
-//                            }
-//
-//                        } else if (activeSubscriptionInfoList.size() == 1) {
-//                            SubscriptionInfo sim = (SubscriptionInfo) activeSubscriptionInfoList.get(0);
-//                            int simSlotIndex = sim.getSimSlotIndex();
-//                            if (simSlotIndex == 0) {
-//                                simManager.isSIM1Ready = true;
-//                                simManager.SIM1OperatorName = (String) sim.getDisplayName();
-//                            } else {
-//                                simManager.isSIM2Ready = true;
-//                                simManager.SIM2OperatorName = (String) sim.getDisplayName();
-//                            }
-//                        }
-//                    }
-//                } else {
-//                    List activeSubscriptionInfoList = subscriptionManager.getActiveSubscriptionInfoList();
-//                    if (activeSubscriptionInfoList.size() >= 2) {
-//                        simManager.isDualSIM = true;
-//
-//                        try {
-//                            SubscriptionInfo sim1 = (SubscriptionInfo) activeSubscriptionInfoList.get(0);
-//                            sim1.getSimSlotIndex();
-//                            simManager.isSIM1Ready = true;
-//                            simManager.SIM1OperatorName = (String) sim1.getDisplayName();
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                        try {
-//                            SubscriptionInfo sim2 = (SubscriptionInfo) activeSubscriptionInfoList.get(1);
-//                            sim2.getSimSlotIndex();
-//                            simManager.isSIM1Ready = true;
-//                            simManager.SIM2OperatorName = (String) sim2.getDisplayName();
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                    } else if (activeSubscriptionInfoList.size() == 1) {
-//                        SubscriptionInfo sim = (SubscriptionInfo) activeSubscriptionInfoList.get(0);
-//                        int simSlotIndex = sim.getSimSlotIndex();
-//                        if (simSlotIndex == 0) {
-//                            simManager.isSIM1Ready = true;
-//                            simManager.SIM1OperatorName = (String) sim.getDisplayName();
-//                        } else {
-//                            simManager.isSIM2Ready = true;
-//                            simManager.SIM2OperatorName = (String) sim.getDisplayName();
-//                        }
-//                    }
-//                }
-//            }
-//        }else{
-//            if (simManager == null) {
-//                simManager = new SIMManager();
-//                TelephonyManager telephonyManager = ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE));
-//
-//                simManager.SIM1OperatorName = null;
-//                simManager.SIM2OperatorName = null;
-//
-//                try {
-//                    simManager.SIM1OperatorName = getSimOperatorNameBySlot(context, "getSimOperatorNameGemini", 0);
-//                    simManager.SIM2OperatorName = getSimOperatorNameBySlot(context, "getSimOperatorNameGemini", 1);
-//                }catch (GeminiMethodNotFoundException e){
-//                    e.printStackTrace();
-//                    try{
-//                        simManager.SIM1OperatorName = getSimOperatorNameBySlot(context, "getSimOperatorName", 0);
-//                        simManager.SIM2OperatorName = getSimOperatorNameBySlot(context, "getSimOperatorName", 1);
-//                    }catch (GeminiMethodNotFoundException e1){
-//                        e1.printStackTrace();
-//                        try {
-//                            simManager.SIM1OperatorName = getSimOperatorNameBySlot(context, "getSimOperatorNameDs", 0);
-//                            simManager.SIM2OperatorName = getSimOperatorNameBySlot(context, "getSimOperatorNameDs", 1);
-//                        }catch (GeminiMethodNotFoundException e2){
-//                            e2.printStackTrace();
-//
-//                            try {
-//                                simManager.SIM1OperatorName = getSimOperatorNameBySlot(context, "getDefault", 0);
-//                                simManager.SIM2OperatorName = getSimOperatorNameBySlot(context, "getDefault", 1);
-//                            }catch (GeminiMethodNotFoundException e3){
-//                                e3.printStackTrace();
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                simManager.isSIM1Ready = telephonyManager.getSimState() == TelephonyManager.SIM_STATE_READY;
-//                simManager.isSIM2Ready = false;
-//
-//                try{
-//                    simManager.isSIM1Ready = getSIMStateBySlot(context, "getSimStateGemini", 0);
-//                    simManager.isSIM2Ready = getSIMStateBySlot(context, "getSimStateGemini", 1);
-//                }catch (GeminiMethodNotFoundException e){
-//                    e.printStackTrace();
-//
-//                    try{
-//                        simManager.isSIM1Ready = getSIMStateBySlot(context, "getSimState", 0);
-//                        simManager.isSIM2Ready = getSIMStateBySlot(context, "getSimState", 1);
-//                    }catch (GeminiMethodNotFoundException e1){
-//                        e1.printStackTrace();
-//                    }
-//                }
-//            }
-//        }
-//
-//        return simManager;
-//    }
-
-
+    
     private fun getSimOperatorNameBySlot(context:Context, predictedMethodName:String, slotID:Int):String?{
         var operatorName:String? = null
 
