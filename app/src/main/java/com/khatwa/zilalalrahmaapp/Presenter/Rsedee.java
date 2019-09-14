4fcc3055ae895c8551e2e9161d82eaad37cc6e65
amin jalal <aminjalalzoom@gmail.com>
2019-09-14 20:56:13 +0200

    fix the carch
    fix the carch of the app

--- app/src/main/java/com/khatwa/zilalalrahmaapp/Presenter/Rsedee.java
+++ app/src/main/java/com/khatwa/zilalalrahmaapp/Presenter/Rsedee.java
@@ -20,74 +20,13 @@ public class Rsedee {
         transactionAmount = "1";
     }
 
-    //USSD start;
-    private void startUSSD(String USSD) {
-        checkPermission();
 
-        Intent intent = new Intent(Intent.ACTION_CALL);
-        intent.setData(ussdToCallableUri(USSD));
-        try {
-            RsedeeActivity rsedeeActivity = new RsedeeActivity();
-            rsedeeActivity.startActivity(intent);
-        }catch (SecurityException e){
-            e.printStackTrace();
-        }
-    }
-
-    private void checkPermission() {
-        RsedeeActivity rsedeeActivity = new RsedeeActivity();
-        if (ActivityCompat.checkSelfPermission(rsedeeActivity.getApplication(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_DENIED) {
-            requestForSpecificPermission(rsedeeActivity);
-        }
-    }
-
-    private void requestForSpecificPermission(RsedeeActivity rsedeeActivity) {
-        ActivityCompat.requestPermissions(rsedeeActivity,new String[Integer.parseInt(Manifest.permission.CALL_PHONE)], 101);
-    }
-
-
-    //Format the USSD.
-    private Uri ussdToCallableUri(String ussd) {
-
-        String uriString = "";
-
-        if (!ussd.startsWith("tel:")) {
-            uriString += "tel:";
-        }
-
-        for (char c : ussd.toCharArray()) {
-
-            if (c == '#') {
-                uriString += Uri.encode("#");
-            } else {
-                uriString += c;
-            }
-        }
-
-        return Uri.parse(uriString);
-    }
 
     //Sudani USSD transfer;
-    public void sendToSudani() {
-        userTelephoneNumber = "0122170298";
-        String USSD = "*333*" + transactionAmount + "*" + userTelephoneNumber + "*0000#";
-        startUSSD(USSD);
-    }
+
 
     //MTN USSD transfer;
-    public void sendToMTN() {
-        userTelephoneNumber = "0967880410";
-        String USSD = "*121*" + userTelephoneNumber + "*" + transactionAmount + "*00000#";
-        startUSSD(USSD);
-    }
 
-    //Zain USSD transfer;
-    public void sendToZain(String code) {
-        userTelephoneNumber = "0967880410";
 
-        String USSD = "*200*" + code + "*" + transactionAmount + "*" + userTelephoneNumber + "*" + userTelephoneNumber + "#";
 
-        //
-        startUSSD(USSD);
-    }
 }
