2d86d712faa7417793677021a10d6dfe92a40e09
MohAbbasher <moh.abbasher@gmail.com>
2019-10-09 15:15:14 +0200

    Add Free Hosting

--- app/src/main/java/com/khatwa/zilalalrahmaapp/Network/ApiClient.java
+++ app/src/main/java/com/khatwa/zilalalrahmaapp/Network/ApiClient.java
@@ -6,9 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "https://5fb0feae.ngrok.io/ZilalApi/";   // todo  URL
+    public static final String BASE_URL = "https://zilaapi.000webhostapp.com/zilalapi/";   // todo  URL
     private static Retrofit retrofit = null;
-    public static final String IMAGE_BASE_URL = "https://5fb0feae.ngrok.io/ZilalApi/images/";  // todo  URL
+    public static final String IMAGE_BASE_URL = "https://zilaapi.000webhostapp.com/zilalapi/images/";  // todo  URL
+
 
     public static Retrofit getClient() {
         if (retrofit == null) {
