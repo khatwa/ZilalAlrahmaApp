0c8d66edb9a5890753f1e37ec2e329d493cc85eb
amin jalal <aminjalalzoom@gmail.com>
2019-11-18 01:15:42 +0200

    delete unuse class

--- app/src/main/java/com/khatwa/zilalalrahmaapp/Notifications/AlarmBootReceiver.java
+++ app/src/main/java/com/khatwa/zilalalrahmaapp/Notifications/AlarmBootReceiver.java
@@ -3,11 +3,17 @@ package com.khatwa.zilalalrahmaapp.Notifications;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
 
 public class AlarmBootReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+        if (Objects.equals(intent.getAction(), "android.intent.action.BOOT_COMPLETED")) {
             //only enabling one type of notifications for demo purposes
             NotificationHelper.scheduleRepeatingElapsedNotification(context);
         }
